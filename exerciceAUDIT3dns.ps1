9f7666c797395f2fe3fd44355563e2b20362f394
Pierre <manginpierre01@gmail.com>
2023-04-11 09:54:06 +0200

    script 3

---
+++ exerciceAUDIT3dns.ps1
@@ -0,0 +1,25 @@
+﻿#====================================================================================
+#NAME: exerciceAUDIT3dns.ps1
+#AUTHOR: MANGIN Pierre
+#DATE: 11/04/2023
+#
+#VERSION 1.0
+#COMMENTS: Afficher dans un fichier texte l'ensemble des enregistrements DNS de type A
+#Requires -Version 2.0
+#====================================================================================
+
+# Obtenir la date actuelle
+$date = Get-Date
+
+# Définir le nom du fichier texte à créer
+$outputFile = "C:\git_cub\contextecubsituation11\auditDNS.txt"
+$zoneDNS = "local.anvers.cub.sioplc.fr"
+
+# Récupérer tous les enregistrements DNS du service DNS
+$dnsRecords = Get-DnsServerResourceRecord -ZoneName $zoneDNS -RRType A
+$output = "Audit DNS - $date`n"
+
+# Écrire les enregistrements DNS dans un fichier texte
+$dnsRecords | Out-File $outputFile
+#Ajouter la date
+$output | Out-File $outputFile -Append
